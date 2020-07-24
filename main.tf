9aa5973c6f09ee70ad2bb7f5298ae67a446111f3
石塚昌也 <ishizuka@MacBook-Pro.local>
2020-07-25 04:12:16 +0900

    add private and public subnet

--- main.tf
+++ main.tf
@@ -11,3 +11,22 @@ resource "aws_vpc" "vpc_for_album" {
     Name = "vpc_for_album"
   }
 }
+
+# subnet
+resource "aws_subnet" "album-public-subnet-1a" {
+  vpc_id = aws_vpc.vpc_for_album.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "ap-northeast-1a"
+  tags = {
+    Name = "album-public-subnet-1a"
+  }
+}
+
+resource "aws_subnet" "album-private-subnet-1c" {
+  vpc_id = aws_vpc.vpc_for_album.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1c"
+  tags = {
+    Name = "album-private-subnet-1c"
+  }
+}
