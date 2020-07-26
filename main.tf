6ee8ed3436806df1f294164059c88d3509fcf8c9
石塚昌也 <ishizuka@MacBook-Pro.local>
2020-07-26 14:36:32 +0900

    add security group

--- main.tf
+++ main.tf
@@ -55,3 +55,48 @@ resource "aws_route_table_association" "public_subnet_association" {
   subnet_id = aws_subnet.album-public-subnet-1a.id
   route_table_id = aws_route_table.table_for_album.id
 }
+
+# Security group
+resource "aws_security_group" "Album-SecurityGroup" {
+  name = "Album-SecurityGroup"
+  description = "Security for Album App"
+  vpc_id = aws_vpc.vpc_for_album.id
+}
+
+# inbound rule
+resource "aws_security_group_rule" "inbound_http" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+
+  security_group_id = "${aws_security_group.Album-SecurityGroup.id}"
+}
+
+resource "aws_security_group_rule" "inbound_https" {
+  type = "ingress"
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+
+  security_group_id = "${aws_security_group.Album-SecurityGroup.id}"
+}
+
+# outbound rule
+resource "aws_security_group_rule" "outbound_http" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+
+  security_group_id = "${aws_security_group.Album-SecurityGroup.id}"
+}
